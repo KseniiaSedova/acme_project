16e0b5cf4a57553dc3c84167c315b5069737aa8a
Sedova Kseniya <sedovakseniya1506@gmail.com>
2023-08-30 16:44:38 +0300

    CBV+Removing

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -4,7 +4,8 @@ from . import views
 from .views import (BirthdayDeleteView,
                     BirthdayListView, 
                     BirthdayCreateView, 
-                    BirthdayUpdateView)
+                    BirthdayUpdateView,
+                    BirthdayDetailView)
 
 
 app_name = 'birthday'
@@ -12,6 +13,7 @@ app_name = 'birthday'
 urlpatterns = [
     path('', views.BirthdayCreateView.as_view(), name='create'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
+    path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
 ]
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
-from .models import Birthday
 from .forms import BirthdayForm
+from .models import Birthday
 from .utils import calculate_birthday_countdown
-
-
-class BirthdayMixin:
-    model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayListView(ListView):
@@ -21,43 +14,27 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
 class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
-    
-    
-class BirthdayCreateView(BirthdayMixin, CreateView):
-    pass
-    
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
-    pass
 
-       
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(request.POST or None,
-                        request.FILES or None, 
-                        instance=instance)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
         )
-        context.update({'birthday_countdown': birthday_countdown})
-    template_name = 'birthday/birthday.html'   
-    return render(request, template_name, context=context)
-
-
-def delete_birthday(request, pk=None):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+        return context 
